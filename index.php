d3838e88fecd4425dabfe476fd3a577a27a7e18c
Tomoaki macbook12i7 <tomoaki.nagahara@gmail.com>
2020-01-16 18:54:46 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * unit-sql:/index.php
+ *
+ * @created   2016-11-29
+ * @version   1.0
+ * @package   unit-sql
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include(__DIR__.'/SQL.class.php');
